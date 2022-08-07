1f2bc6e840ff1eb5571f6b2b5aa9021c35eb01e0
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-07 14:48:37 +0900

    矩形は鳴らした。

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
-<template>
-  <div>
-    <NuxtWelcome />
-  </div>
+<template lang="pug">
+div
+  NuxtPage
 </template>
+
+<script lang="ts">
+const eventName =
+  typeof document.ontouchend !== "undefined" ? "touchend" : "mouseup";
+document.addEventListener(eventName, initAudioContext);
+function initAudioContext() {
+  document.removeEventListener(eventName, initAudioContext);
+  // wake up AudioContext
+  useAudioContext().value.resume();
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+div
+  button(@click="play") hello
+</template>
+
+<script setup lang="ts">
+const play = () => {
+  const osc = useAudioContext().value.createOscillator();
+  osc.type = "square";
+  osc.frequency.value = 440 * Math.pow(Math.pow(2, 1 / 12), 1);
+  osc.connect(useAudioContext().value.destination);
+  osc.start();
+  setTimeout(() => {
+    osc.stop();
+    const osc2 = useAudioContext().value.createOscillator();
+    osc2.type = "square";
+    osc2.frequency.value = 440 * Math.pow(Math.pow(2, 1 / 12), 3);
+    osc2.connect(useAudioContext().value.destination);
+    osc2.start();
+  }, 1000);
+};
+</script>
